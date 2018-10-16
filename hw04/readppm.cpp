e99e1904f80447668d016222b49deeca446e6b5b
jerredshifflett <jshiffl1@my.hpu.edu>
2018-10-16 12:13:00 -1000

    I can not use the %d width and height for simple math

--- hw04/readppm.cpp
+++ hw04/readppm.cpp
@@ -6,7 +6,8 @@ void readPPM()
 char string[81];
 int* width;
 int* height;
-
+int* maxPixel;
+int* bytes;
 FILE* fpPPM = fopen("test.ppm", "rb");
 if(fpPPM==NULL){
 	printf("didnt work\n");
@@ -15,12 +16,34 @@ fscanf(fpPPM, "%s", string);
 printf("%s\n",string);
 //read width
 fscanf(fpPPM, "%d", &width);
-printf("%d width\n", width);
+printf("%d width\n", width);//test
 
 
 //read width
 fscanf(fpPPM, "%d", &height);
-printf("%d width\n", height);
+printf("%d height\n", height);//test
+
+//read maxPixel
+fscanf(fpPPM, "%d", &maxPixel);
+printf("%d Max Pixels\n", maxPixel);//test
+
+//read binary pixel data
+//int size = width * height * 3;
+printf("%d size of this thing", (*width) * (*height) * 3 );
+
+
+
+//fread(array, sizeof(int), 128, fpPPM);
+//printf("%c",array);
+//printf("%d  das bytes", nbytes);
+//printf(" bytes\n",&bytes);
 fclose(fpPPM);
+
+//\nbinaryjunk
+
+//read imagePixels
+//int size = *height * *width * 3;
+
+
 
 }
